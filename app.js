b7f19a66359bbc41eba10733c31859a94463d41d
Marvin Scheffold <m@marvinscheffold.com>
2024-09-04 10:34:03 +0200

    first

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+function wildcardRoute(req, res, next) {
+  const text = `url: ${req.url}, originalUrl: ${req.originalUrl}, host: ${req.host}, headersHost ${req.headers.host}, localPort: ${req.socket.localPort}, remotePort: ${req.socket.remotePort}`;
+  res.send(text);
+}
+
+app.get("/", wildcardRoute);
+app.get("/*", wildcardRoute);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
